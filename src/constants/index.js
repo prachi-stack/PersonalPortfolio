0096f8cdf9c23a35bdd5f4c3247567ff0b44caa0
prachi-stack <deopaprachi60@gmail.com>
2024-12-31 18:12:48 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -94,7 +94,7 @@ import {
         },
       ],
       image: portfolio,
-      source_code_link: "https://github.com/",
+      source_code_link: "https://personalportfolio-eqjb.onrender.com",
     },
     {
       name: "Mental Health Chatbot",
